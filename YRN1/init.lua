0de883a71c8ba0ecd6fe5dd38a3fc2b3a5e8f014
Jeroen Baekelandt <yrn001@gmail.com>
2020-09-13 13:57:23 +0200

    Trying my own unit. Copy of Offset.

---
+++ YRN1/init.lua
@@ -0,0 +1,13 @@
+local units = {
+    {category="YRN1"},
+    {title="Thru", moduleName="Thru",keywords="utility"},
+  }
+  
+  return {
+    title = "YRN1",
+    name = "yrn1",
+    contact = "via O|D Forums @yrn1",
+    keyword = "YRN1",
+    units = units
+  }
+  
