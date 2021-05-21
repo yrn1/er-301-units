fddfe8dac6c3a0211bc2432efab69e8e2eb88fc6
Jeroen Baekelandt <dev@yrn1.com>
2021-05-21 17:32:36 +0200

    move into YRN1

--- YRN1/toc.lua
+++ YRN1/toc.lua
@@ -3,10 +3,8 @@ return {
     author = "YRN1",
     name = "yrn1",
     units = {{
-        -- Title and category control how this unit appears in the unit browser.
         title = "Filter Delay",
         category = "YRN1",
-        -- Which lua module contains the unit definition?
         moduleName = "FilterDelay"
     }}
 }
