bba2d0e0294716f96e5edc2653248b0401653ef7
Jeroen Baekelandt <dev@yrn1.com>
2021-05-23 17:26:55 +0200

    License

--- YRN1/FDN.lua
+++ YRN1/FDN.lua
@@ -1,3 +1,26 @@
+-- Copyright (c) 2021, Jeroen Baekelandt
+-- All rights reserved.
+-- Redistribution and use in source and binary forms, with or without modification,
+-- are permitted provided that the following conditions are met:
+-- * Redistributions of source code must retain the above copyright notice, this
+--   list of conditions and the following disclaimer.
+-- * Redistributions in binary form must reproduce the above copyright notice, this
+--   list of conditions and the following disclaimer in the documentation and/or
+--   other materials provided with the distribution.
+-- * Neither the name of the {organization} nor the names of its
+--   contributors may be used to endorse or promote products derived from
+--   this software without specific prior written permission.
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+-- ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+-- WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+-- DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+-- ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+-- (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+-- LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+-- ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+-- (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+--
 local Class = require "Base.Class"
 local Unit = require "Unit"
 local Encoder = require "Encoder"
@@ -168,17 +191,17 @@ function FDN:createAdapterControl(name)
 end
 
 function FDN:positive(name, sum)
-    local negation = self:addObject(name.."r", app.ConstantGain())
+    local negation = self:addObject(name .. "r", app.ConstantGain())
     negation:hardSet("Gain", 1.0)
     connect(negation, "Out", sum, "Right")
     return negation
 end
 
 function FDN:negative(name, sum)
-local negation = self:addObject(name.."r", app.ConstantGain())
-negation:hardSet("Gain", -1.0)
-connect(negation, "Out", sum, "Right")
-return negation
+    local negation = self:addObject(name .. "r", app.ConstantGain())
+    negation:hardSet("Gain", -1.0)
+    connect(negation, "Out", sum, "Right")
+    return negation
 end
 
 function FDN:createEq(name, high, mid, low)
